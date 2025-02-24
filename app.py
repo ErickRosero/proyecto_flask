b0bb6524e43243fe56e9b2405f924f87283aac38
ErickRosero <es.roseroh@uea.edu.ec>
2025-02-23 19:48:56 -0500

    Implementación de plantillas Jinja2

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask
+# app.py
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return 'Bienvenido a mi primera aplicación Flask!'
+    return render_template('index.html', title='Inicio')
 
 @app.route('/usuario/<nombre>')
 def usuario(nombre):
-    return f'Bienvenido, {nombre}!'
+    return render_template('usuario.html', title='Perfil', nombre=nombre)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='Acerca de')
 
 if __name__ == '__main__':
     app.run(debug=True)
